1fbed21c77420ac8c8b8c26481815b820286e3b3
Xiao Chen <chenx376@outlook.com>
2018-04-20 15:06:11 -0700

    Update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Mobile_App
+
+This is backend server which runs in Google Cloud Platform
+
+To Install All requires dependencies:
+
+```
+npm install
+```
+
+To Run the server:
+```
+node server.js
+```
--- server.js
+++ server.js
@@ -10,6 +10,14 @@ var port = 8585;
 var app = express();
 var server = http.createServer(app);
 
+var text = "{code:0, errMsg:'',data:{'Apple': {information}, 'Orange': {information}}}";
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  next();
+});
 
 app.use(bodyParser.json({limit: '1mb'}));
 app.use(bodyParser.urlencoded({
@@ -19,8 +27,8 @@ app.use(bodyParser.urlencoded({
 // Need to extend
 app.post('/photo', function (req, res) {
     console.log(req.body);
-    res.send(req.body);
+    res.send(text);
 });
 
 server.listen(port);
-console.log('Sever Started');
+console.log('Server Started');
